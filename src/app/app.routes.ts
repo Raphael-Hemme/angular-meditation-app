f9a42562f7eee4e3483b5149d21786c6fbe9f057
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2024-08-12 15:14:51 +0200

    feat: Add new pages and components
    This commit adds the following new pages and components:
    - practice-page.component
    - settings-page.component
    - not-found-page.component
    - home-page.component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PracticePageComponent } from './pages/practice-page/practice-page.component';
+import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'practice',
+    component: PracticePageComponent,
+  },
+  {
+    path: 'settings',
+    component: SettingsPageComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent,
+  },
+];
---
+++ src/app/pages/practice-page/practice-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-practice-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './practice-page.component.html',
+  styleUrl: './practice-page.component.scss'
+})
+export class PracticePageComponent {
+
+}
