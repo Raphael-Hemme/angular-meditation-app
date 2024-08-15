af57e3ec9f7de7ca739ebaa8f2bd1753b14c7760
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2024-08-15 13:25:21 +0200

    cosmetic: work on styling and add logic to handle time ranges of TimerInputComponent

--- src/app/components/timer-input/timer-input.component.ts
+++ src/app/components/timer-input/timer-input.component.ts
@@ -17,18 +17,26 @@ export class TimerInputComponent {
   public minutes = signal(5);
 
   public increaseHours(): void {
-    this.hours.update((value) => value + 1);
+    this.hours.update((value) => {
+      return value < 23 ? value + 1 : 0;
+    });
   }
 
   public decreaseHours(): void {
-    this.hours.update((value) => value - 1);
+    this.hours.update((value) => {
+      return value > 0 ? value - 1 : 0;
+    });
   }
 
   public increaseMinutes(): void {
-    this.minutes.update((value) => value + 1);
+    this.minutes.update((value) => {
+      return value < 59 ? value + 1 : 0;
+    });
   }
 
   public decreaseMinutes(): void {
-    this.minutes.update((value) => value - 1);
+    this.minutes.update((value) => {
+      return value > 0 ? value - 1 : 0;
+    });
   }
 }
